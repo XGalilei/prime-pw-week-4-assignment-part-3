459ee9364ad3f2276dae4a48afffe670544c437a
XGalilei <ianbush1@gmail.com>
2021-08-05 11:58:07 -0500

    Added remaining two mandatory methods and began testing code

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,3 +7,23 @@ function addItem(item) {
   basket.push(item);
   return true;
 }
+
+function listItems() {
+  for(item of basket) {
+    console.log(item);
+  }
+}
+
+function empty() {
+  while(basket.length > 0) {
+    basket.pop();
+  }
+}
+
+console.log(`==Testing addItem==`);
+console.log(`==Testing listItems==`);
+
+console.log(`==Testing empty==`);
+console.log(`Emptying basket:`);
+empty();
+console.log(`Basket is now ${basket}`);
